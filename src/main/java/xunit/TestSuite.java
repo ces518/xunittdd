6d9c4877324475cf6c3ca1912d85731dbcb567ad
me <me.ncucu@kakaocorp.com>
2021-10-12 02:17:29 +0900

    refactor : TestSuite 생성시 리플렉션 기반 자동화 적용

--- src/main/java/xunit/TestSuite.java
+++ src/main/java/xunit/TestSuite.java
@@ -1,19 +1,33 @@
 package xunit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestSuite implements Test {
 
     List<Test> tests = new ArrayList<>();
 
+    public TestSuite() {
+    }
+
+    public TestSuite(Class<? extends Test> clazz) {
+        Arrays.stream(clazz.getMethods())
+            .filter(m -> m.getName().startsWith("test"))
+            .forEach(t -> {
+                try {
+                    add(clazz.getConstructor(String.class).newInstance(t.getName()));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+    }
+
     public void add(Test test) {
         tests.add(test);
     }
 
     public void run(TestResult result) {
-        tests.forEach(t -> {
-            t.run(result);
-        });
+        tests.forEach(t -> t.run(result));
     }
 }
