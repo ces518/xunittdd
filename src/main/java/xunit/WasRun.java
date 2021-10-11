e2012f3a572e53315cc642dda9e8a84ecccac7e6
me <me.ncucu@kakaocorp.com>
2021-10-12 00:48:41 +0900

    feat : 테스트 메소드 실행전 setUp 메소드가 실행된다

--- src/main/java/xunit/WasRun.java
+++ src/main/java/xunit/WasRun.java
@@ -3,9 +3,15 @@ package xunit;
 public class WasRun extends TestCase {
 
     public boolean wasRun;
+    public boolean wasSetUp;
 
     public WasRun(String name) {
         super(name);
+    }
+
+    @Override
+    public void setUp() {
+        this.wasSetUp = true;
     }
 
     public void testMethod() {
