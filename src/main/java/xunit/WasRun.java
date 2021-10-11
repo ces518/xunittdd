b88125e9ed9ddfc75e50666b64839fe213f82a60
me <me.ncucu@kakaocorp.com>
2021-10-12 00:54:17 +0900

    feat : 테스트 메소드의 호출순서를 확인할 수 있는 log 필드 추가

--- src/main/java/xunit/WasRun.java
+++ src/main/java/xunit/WasRun.java
@@ -4,6 +4,7 @@ public class WasRun extends TestCase {
 
     public boolean wasRun;
     public boolean wasSetUp;
+    public String log;
 
     public WasRun(String name) {
         super(name);
@@ -12,9 +13,11 @@ public class WasRun extends TestCase {
     @Override
     public void setUp() {
         this.wasSetUp = true;
+        log = "setUp";
     }
 
     public void testMethod() {
         this.wasRun = true;
+        log += " testMethod";
     }
 }
